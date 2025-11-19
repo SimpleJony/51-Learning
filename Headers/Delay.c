49f5858eeeb6c7a9a51c74d6d3e8f09c17e5507d
SimpleJony <jonylovesmile@outlook.com>
2025-11-19 23:19:20 +0800

    Update header files *Just realize my frequency is 11.0592, not 12

--- Headers/Delay.c
+++ Headers/Delay.c
@@ -1,12 +1,11 @@
 #include <Atmel/REGX52.H>
 
-void Delay(unsigned int x)
+void Delay(unsigned int x)		//@11.0592MHz
 {
 	unsigned char i, j;
-	while(x--)
-	{
+	while(x--){
 		i = 2;
-		j = 239;
+		j = 199;
 		do
 		{
 			while (--j);
